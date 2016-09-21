05b0b8100a26f357a79657d57ef6cbbe87e6af66
Aditya <agulava@ncsu.edu>
2016-09-20 22:47:58 -0400

    Organised Directories

---
+++ P1/codes/8. page fault service time/minibench.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main()
+{
+	unsigned cycles_high, cycles_high1, cycles_low, cycles_low1;
+	uint64_t start, end;
+
+	asm volatile ("cpuid\n\t"
+		"rdtsc\n\t"
+		"mov %%edx, %0\n\t"
+		"mov %%eax, %1\n\t"
+		: "=r" (cycles_high), "=r" (cycles_low)
+		:: "%rax", "%rbx", "%rcx", "%rdx");
+
+	// Enter Code here
+
+	asm volatile ("rdtscp\n\t"
+		"mov %%edx, %0\n\t"
+		"mov %%eax, %1\n\t"
+		"cpuid\n\t"
+		: "=r" (cycles_high1), "=r" (cycles_low1)
+		:: "%rax", "%rbx", "%rcx", "%rdx");
+
+	start = ( ((uint64_t)cycles_high << 32) | cycles_low );
+	end = ( ((uint64_t)cycles_high1 << 32) | cycles_low1 );
+	printf("%llu \n", (end-start));
+
+	return k;
+}
