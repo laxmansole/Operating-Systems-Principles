95346608c663861c9e050a9e0275ea0268abd8a9
Aditya <agulava@ncsu.edu>
2016-09-20 20:10:13 -0400

    Updating

--- git_all.sh
+++ git_all.sh
@@ -3,6 +3,7 @@ git config --global credential.helper 'cache --timeout 7200'
 if [ "$1" == "" ]; then
 	echo "Usage: ./git_all.sh \"message\""
 else
+	git pull
 	git add .
 	git commit -m "$1" *
 	git push
