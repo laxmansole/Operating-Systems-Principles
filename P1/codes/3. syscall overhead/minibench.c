1b87480141bc69a05f2b0369b7f847c18856a069
Aditya <agulava@ncsu.edu>
2016-09-21 21:02:47 -0400

    Added 3 experiments in folder 3

--- P1/codes/3. syscall overhead/minibench.c
+++ P1/codes/3. syscall overhead/minibench.c
@@ -1,10 +1,16 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <unistd.h>
+
 typedef long long uint64_t;
+
 int main()
 {
 	unsigned cycles_high, cycles_high1, cycles_low, cycles_low1;
 	uint64_t start, end;
 
+	int pdes[2] = {10, 20};
+	char buf[100];
+	 
 	asm volatile ("cpuid\n\t"
 		"rdtsc\n\t"
 		"mov %%edx, %0\n\t"
@@ -13,6 +19,7 @@ int main()
 		:: "%rax", "%rbx", "%rcx", "%rdx");
 
 	// Enter Code here
+	pipe(pdes);
 
 	asm volatile ("rdtscp\n\t"
 		"mov %%edx, %0\n\t"
@@ -24,6 +31,14 @@ int main()
 	start = ( ((uint64_t)cycles_high << 32) | cycles_low );
 	end = ( ((uint64_t)cycles_high1 << 32) | cycles_low1 );
 	printf("%llu \n", (end-start));
+
+	if ( fork() == 0 ){
+		close(pdes[1]); 
+		read(pdes[0], buf, 100); 									/* read from parent */
+	} else{
+		close(pdes[0]); 
+		write(pdes[1], "this is written\n", 16); 					/* write to child */
+	}
 
 	return 0;
 
